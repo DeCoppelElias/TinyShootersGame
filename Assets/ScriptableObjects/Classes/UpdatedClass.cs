0cee0bb37e5b9e51a991439271259138957e718f
EliasDeCoppel <elias.decoppel@gmail.com>
2025-06-05 18:33:13 +0200

    First commit: the start of the class update, details are provided in the UpdatedClass file.

---
+++ Assets/ScriptableObjects/Classes/UpdatedClass.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class is the latest version of the class system in Tiny Shooters. 
+/// Instead of forcing set values when a player selects a class, I want to define how much is added or subtracted from player stats. 
+/// This is the first step in allowing more random power updates inbetween levels, allowing for more unique combinations.
+/// </summary>
+public class UpdatedClass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
